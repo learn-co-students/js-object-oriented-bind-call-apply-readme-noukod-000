b2c8a56a844883a7d66dd6d12cbe9a88ce0859aa
Gilbert Torchon <gilbert@noukod.com>
2019-02-09 15:30:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+let sally = { name: 'Sally' };
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+greet.call(sally, 'Terry', 'George');
+// Hi Terry and George, my name is Sally!
