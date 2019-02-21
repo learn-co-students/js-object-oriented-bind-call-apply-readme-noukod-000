1f37bb9fd661b831e3f221a87e13f691eb0bb259
Josue Abraham <josuea@noukod.com>
2019-02-21 17:33:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
