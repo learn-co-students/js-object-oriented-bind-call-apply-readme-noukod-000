39ce070951c49cb58e93e6a7784bf838128d4af3
Billy Joseph <ingbillyjoseph@gmail.com>
2019-11-06 16:40:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+class Event {
+  constructor(title, keywords) {
+      this.title = title;
+      this.keywords = keywords;
+  }
+}
+
+class User {
+  constructor(name, interests) {
+      this.name = name;
+      this.interests = interests;
+  }
+
+  matchInterests(event) {
+      return event.keywords.some(function(word) {
+          return this.interests.includes(word);
+      });
+  }
+}
